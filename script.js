33c631f62f331f0224d2207087063eb49ccf087a
MikolHutnik <mikolhutnik@gmail.com>
2018-07-11 15:26:41 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿
+
+function openWinOne() {
+    window.open("https://github.com/MikolHutnik/Tuscany2");
+}
+
+function openWinTwo() {
+    window.open("https://github.com/MikolHutnik/VirtualPet");
+}
+
+function openWinThree() {
+    window.open("https://github.com/MikolHutnik/LuckyNumbers");
+}
